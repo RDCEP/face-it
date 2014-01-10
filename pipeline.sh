094cab1d7494a440aadc086121ff101c93c2af55
ketancmaheshwari <ketancmaheshwari@gmail.com>
2014-01-10 15:03:49 -0600

    adding pipeline

---
+++ pipeline.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+set -x
+#set -e
+
+quaduijar="./quadui/quadui-1.2.1-SNAPSHOT-Beta13.jar"
+acmouijar="./acmo/acmoui-1.2-SNAPSHOT-beta4.jar"
+
+#for zipfile in `find faceit-eastafrica-data -iname "Field*.zip"`
+for zipfile in `find FIXED_ACE_DOME -iname "Field*.zip"`
+do 
+    dirname=`dirname $zipfile`
+
+    surveydata="$dirname/Survey_data_import.zip"
+    linkagedata="$dirname/Survey_data_import_Linkage.aceb"
+    fielddata="$dirname/Field_Overlay.zip"
+    strategydata="$dirname/Seasonal_strategy.zip"
+    outdir="${dirname}_outdir"
+    
+    echo "Starting ..."
+    sleep 1
+    
+    echo " Run quadui ... "
+    time -p java -Xms256m -Xmx768m -jar $quaduijar -cli -zip -clean -f -s -D $surveydata ' '  $fielddata $strategydata $outdir
+    #time -p java -Xms256m -Xmx768m -jar $quaduijar -cli -zip -clean -f -s -D $surveydata $linkagedata $fielddata $strategydata $outdir
+    
+    #Copy DSSAT Auxiliary file bundle to outdir/DSSAT
+    cp dssat_aux.tgz $outdir/DSSAT
+    
+    #unzip quadoutdir/DSSAT and the DSSAT_Input.zip
+    cd $outdir/DSSAT
+    unzip DSSAT_Input.zip
+
+    ##untar the auxiliary package here and move all files in DSSAT dir
+    tar zxf dssat_aux.tgz && mv dssat_aux/* .
+     
+    echo "quadui done,  run DSSAT ... "
+    #sleep 1
+    #
+    for i in *.SNX
+    do
+        ./DSCSM045.EXE A $i
+        rm -f SoilOrg.OUT PlantGro.OUT OVERVIEW.OUT LUN.LST DSSAT45.INP DSSAT45.INH
+        mkdir -p ${i}_dir
+        mv *.OUT ${i}_dir
+    done 
+    #
+    cd -
+    #
+    echo " DSSAT done, Run acmoui ..."
+    for i in ${outdir}/DSSAT/*.SNX
+    do
+        cp ${i}_dir/* ${outdir}/DSSAT
+        java -jar $acmouijar -cli -dssat ${outdir}/DSSAT
+    done
+    #
+    echo "Done. Check out results in $outdir/DSSAT"
+done
+
